cfd48d3cf1767a2885c7bd01e24d3af67c0b86aa
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-01-15 15:05:19 +0100

    modification "models " "Books" conformément à l'api du projet. création du schema correspondant

--- models/Book.js
+++ models/Book.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({
+const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },  
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  author : { type: String, required: true },
   imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('Book', thingSchema);
