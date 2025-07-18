7720cf89fce2f12388022edb1572eb3d35d5177e
GuillaumeB75 <guillaume.bezie@yahoo.com>
2025-07-18 08:58:30 +0200

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,59 @@
+// Importation du module bcrypt pour le hachage des mots de passe.
 const bcrypt = require('bcrypt');
+// Importation de jsonwebtoken pour créer et gérer des tokens d'authentification.
 const jwt = require('jsonwebtoken');
 
-
+// Importation du modèle User, qui est un schéma Mongoose pour les utilisateurs.
 const User = require('../models/User');
 
+// Fonction pour l'inscription d'un nouvel utilisateur.
 exports.signup = (req, res, next) => {
+    // Hashage du mot de passe 
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
+        // Création d'un nouvel utilisateur avec l'email reçu et le mot de passe haché.
         const user = new User({
           email: req.body.email,
           password: hash
         });
+        // Sauvegarde de l'utilisateur dans la base de données.
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' })) // Succès : utilisateur créé.
+          .catch(error => res.status(400).json({ error })); // Erreur : problème lors de l'enregistrement.
       })
+      // Erreur de hachage.
       .catch(error => res.status(500).json({ error }));
   };
 
-  exports.login = (req, res, next) => {
+// Fonction pour connecter un utilisateur existant.
+exports.login = (req, res, next) => {
+    // Recherche de l'utilisateur par email.
     User.findOne({ email: req.body.email })
         .then(user => {
+            // Si l'utilisateur n'existe pas, retourner une erreur.
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
+            // Comparaison du mot de passe envoyé avec le mot de passe haché stocké.
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
+                    // Si la comparaison échoue, retourner une erreur.
                     if (!valid) {
                         return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
+                    // Si la comparaison est réussie, retourner l'ID utilisateur et un token d'authentification.
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
                             'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
+                            { expiresIn: '24h' } // Le token expire après 24 heures.
                         )
                     });
                 })
+                // Erreur pendant la vérification du mot de passe.
                 .catch(error => res.status(500).json({ error }));
         })
+        // Erreur lors de la recherche de l'utilisateur.
         .catch(error => res.status(500).json({ error }));
  };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,27 @@
+
 const jwt = require('jsonwebtoken');
- 
+
+// Exportation d'un middleware qui sera utilisé pour protéger les routes
 module.exports = (req, res, next) => {
    try {
+       // Extraction du token du header 'Authorization' de la requête entrante
        const token = req.headers.authorization.split(' ')[1];
+
+       // Vérification du token avec une clé secrète; la fonction verify lève une erreur si le token est invalide
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+
+       // Extraction de l'identifiant utilisateur (userId) des données du token décodé
        const userId = decodedToken.userId;
+
+       // Ajout de l'identifiant utilisateur à l'objet de requête 
        req.auth = {
            userId: userId
        };
+
+       // Passage à la prochaine fonction middleware dans la pile
 	next();
    } catch(error) {
+       // Si une erreur se produit (par exemple, si le token est invalide), envoi d'une réponse avec le statut 401 Unauthorized
        res.status(401).json({ error });
    }
 };
